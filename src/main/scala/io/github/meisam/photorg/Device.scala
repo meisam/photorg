eda341e056ba5b2ac7977370d80e49d97eacee86
Meisam F.S <meisam.fathi@gmail.com>
2022-07-04 14:39:00 -0700

    Simplify the algebra to pull and push files
    Use cat's `traverse` to pull and push media files
    from and to android devices

--- src/main/scala/io/github/meisam/photorg/Device.scala
+++ src/main/scala/io/github/meisam/photorg/Device.scala
@@ -15,6 +15,8 @@ package io.github.meisam.photorg
 
 import cats.free.Free
 import cats.free.Free.liftF
+import cats.syntax.all.*
+import cats.syntax.traverse
 
 type ErrorMessages = String
 type DeviceId = String
@@ -48,35 +50,21 @@ def pushMediaFile(deviceId: DeviceId)(
 ): AndroidDevice[MediaFile] =
   liftF[AndroidDeviceA, MediaFile](PushMediaFile(deviceId, mediaFile))
 
-extension (list: List[AndroidDevice[MediaFile]])
-  def sequence: AndroidDevice[List[MediaFile]] =
-    val zero = Free.pure[AndroidDeviceA, List[MediaFile]](List.empty)
-    list.foldLeft(zero)((accum, file) =>
-      accum.flatMap(tail => file.map(g => g :: tail))
-    )
-
-def program(
+def backupMediaFilesApp(
     sourceDeviceId: DeviceId,
     targetDeviceId: DeviceId,
     cameraPath: String
 ): AndroidDevice[List[MediaFile]] =
-  val zero: AndroidDevice[List[MediaFile]] =
-    Free.pure[AndroidDeviceA, List[MediaFile]](List.empty)
-  val pulledFiles: AndroidDevice[List[MediaFile]] =
-    getMediaFiles(sourceDeviceId, cameraPath)
-      .map(files => files.map(pullMediaFile(sourceDeviceId)))
-      .flatMap(_.sequence)
-  val pushedFiles: AndroidDevice[List[MediaFile]] =
-    pulledFiles
-      .map(files => files.map(pushMediaFile(targetDeviceId)))
-      .flatMap(_.sequence)
-  pushedFiles
+  val filePullingFunction = pullMediaFile(sourceDeviceId)
+  val filePushingFunction = pushMediaFile(targetDeviceId)
+  for
+    mediaFiles <- getMediaFiles(sourceDeviceId, cameraPath)
+    filesToBePulled <- mediaFiles.traverse(filePullingFunction)
+    filesToBePushed <- filesToBePulled.traverse(filePushingFunction)
+  yield filesToBePushed
 
-import cats.arrow.FunctionK
 import cats.{Id, ~>}
-import scala.collection.mutable
-
-val impureCompiler: AndroidDeviceA ~> Id = new:
+val mockCompiler: AndroidDeviceA ~> Id = new:
   var files = List("Image1.jpg", "image2.CR2", "imag3.MOV", "image4.mp4")
   def apply[A](fa: AndroidDeviceA[A]): Id[A] =
     fa match
@@ -92,4 +80,4 @@ val impureCompiler: AndroidDeviceA ~> Id = new:
 
 @main
 def freeMonadRun =
-  val x = program("1234", "abcd", "/DCIM").foldMap(impureCompiler)
+  val x = backupMediaFilesApp("1234", "abcd", "/DCIM").foldMap(mockCompiler)
